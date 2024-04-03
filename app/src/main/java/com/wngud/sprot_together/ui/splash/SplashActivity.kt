96feaabb82b93fd9f432301b19e6f8114db0e4d1
이주형 <70958782+dlwngud@users.noreply.github.com>
2024-04-03 17:46:40 +0900

    [feat] splash activity 추가

---
+++ app/src/main/java/com/wngud/sprot_together/ui/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.wngud.sprot_together.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.wngud.sprot_together.databinding.ActivitySplashBinding
+import com.wngud.sprot_together.ui.onboard.OnboardingActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 2초 후 메인 화면으로 이동
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, OnboardingActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
