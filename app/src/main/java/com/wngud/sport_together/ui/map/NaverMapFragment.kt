6f8f661417a9ab63848033116e114327f42085df
이주형 <70958782+dlwngud@users.noreply.github.com>
2024-04-04 22:08:32 +0900

    [feat] 지도상에 현재 위치 표시

--- app/src/main/java/com/wngud/sport_together/ui/map/NaverMapFragment.kt
+++ app/src/main/java/com/wngud/sport_together/ui/map/NaverMapFragment.kt
@@ -1,20 +1,82 @@
 package com.wngud.sport_together.ui.map
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
+import com.wngud.sport_together.R
 import com.wngud.sport_together.databinding.FragmentMapBinding
 
-class NaverMapFragment : Fragment() {
+class NaverMapFragment : Fragment(), OnMapReadyCallback {
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
 
     private lateinit var binding: FragmentMapBinding
+
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
+
+    private val permissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMapBinding.inflate(layoutInflater, container, false)
+
+        if (!hasPermission()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                permissions,
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            initMapView()
+        }
+
         return binding.root
+    }
+
+    private fun initMapView() {
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+
+        mapFragment.getMapAsync(this)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+    }
+
+    private fun hasPermission(): Boolean {
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(requireContext(), permission)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+
+        naverMap.uiSettings.isLocationButtonEnabled = true
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
     }
 }
